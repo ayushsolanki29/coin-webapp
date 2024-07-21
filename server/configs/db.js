3bf040db5570da1aec0d4f08eb8c01edc81ca209
Ayush Solanki <ayushsolanki2901@gmail.com>
2024-07-21 18:15:59 +0530

    update

--- server/configs/db.js
+++ server/configs/db.js
@@ -2,6 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect("mongodb://localhost:27017/coin-generator")
+    .connect("mongodb+srv://ayush:ayush2901@cluster0.mxtcvv0.mongodb.net/coin-genrator")
+    // .connect("mongodb://localhost:27017/coin-generator")
     .then(() => console.log("DB connected"));
 };
